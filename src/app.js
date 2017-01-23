9c728123722ddfac82e41db146d3fcc649ef682e
Robert Rees <shudderfix@gmail.com>
2017-01-23 23:01:54 +0000

    Switches from directive to component

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 
 console.log('Hello world');
 
+function ScorerController() {
+    var vm = this;
+    vm.players = [];
+}
+
+const scorerTemplate = `
+<p>Hello from the <em>component</em></p>
+`;
+
 angular.module('app', [])
     .controller('MainCtrl', [function() {
         var vm = this;
         console.log('Main Controller created');
         this.message = "Hello from the controller";
     }])
-    .directive('scorer', [function() {
-        return {
-            'restrict': 'E',
-            'template': 'Hello from the directive'
-        }
-    }]);
+    .component('scorer', {
+        'restrict': 'E',
+        'template': scorerTemplate,
+        controller: ScorerController
+    });
