70fb8c9949e87f7162a3b19e967e7bc100abec70
Robert Rees <shudderfix@gmail.com>
2017-01-24 07:07:53 +0000

    Players can be added to the list

--- src/app.js
+++ src/app.js
@@ -4,10 +4,24 @@ console.log('Hello world');
 function ScorerController() {
     var vm = this;
     vm.players = [];
+
+    vm.addPlayer = function(name) {
+        console.log(name);
+        vm.players.push(name);
+        vm.playerName = "";
+    }
 }
 
 const scorerTemplate = `
 <p>Hello from the <em>component</em></p>
+<form>
+<input type="text" placeholder="Player name" ng-model="$ctrl.playerName">
+<button ng-click="$ctrl.addPlayer($ctrl.playerName)">Add player</button>
+</form>
+
+<div class="player-data" ng-repeat="player in $ctrl.players">
+<p>{{player}}</p>
+</div>
 `;
 
 angular.module('app', [])
